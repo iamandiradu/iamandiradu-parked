36d5ba679f7e773ff19b5af9deb428e97bfb4885
freebeeromg <iamandiradustefan@gmail.com>
2019-12-01 08:12:37 +0200

    add intellibin demo

---
+++ intellibin/src/data/materials.js
@@ -0,0 +1,11 @@
+export const materials = {
+  aluminium: "yellow",
+  "soft plastic": "yellow",
+  "hard plastic": "yellow",
+  metal: "yellow",
+  carton: "blue",
+  paper: "blue",
+  cardboard: "blue",
+  glass: "green",
+  circuit: "cyan"
+};
